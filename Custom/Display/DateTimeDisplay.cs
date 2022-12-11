461ee4e6fd818215b52515146eb5ba76b038f258
Adhitya Bagus Wicaksono <adhityabagus80@gmail.com>
2022-12-11 22:44:10 +0700

    Date Time Done!

--- Custom/Display/DateTimeDisplay.cs
+++ Custom/Display/DateTimeDisplay.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DateTimeDisplay : MonoBehaviour
 {
+    public GameObject dateTimeDisplay;
+    private string time, date;
+
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         
+=======
+
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        date = System.DateTime.Now.ToString("dd MMMM yyyy");
+        time = System.DateTime.Now.ToString("HH:mm:ss");
+        dateTimeDisplay.GetComponent<Text>().text = time + "\n" + date;
     }
 }
