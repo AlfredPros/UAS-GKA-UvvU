2f56fd9edfb39119d09a59f623c85007241185a6
IsRylo <91057829+IsRylo@users.noreply.github.com>
2022-12-11 21:09:25 +0800

    Test 2 From Loysing

--- Custom/Display/DateTimeDisplay.cs
+++ Custom/Display/DateTimeDisplay.cs
@@ -7,7 +7,7 @@ public class DateTimeDisplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello Test");
     }
 
     // Update is called once per frame
