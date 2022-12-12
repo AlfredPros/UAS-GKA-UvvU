8ea81347491b4e452ef63f41bfe8d3c92436fd7b
AlfredPros <alfredkuhlman@yahoo.co.id>
2022-12-13 00:49:02 +0700

    YBot Animations and Movement

--- Custom/Player/PlayerControl.cs
+++ Custom/Player/PlayerControl.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Testing.
-        // testing 2.
-        // testing 3.
-
+        anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /// Run: Input.GetAxis("Fire3")
+        /// Jump: Input.GetAxis("Submit")
+
+        float translation = Input.GetAxis("Vertical");
+
+        // Jump Check
+        if (Input.GetAxis("Submit") != 0) {
+            anim.SetBool("isJump", true);
+        }
+        else {
+            anim.SetBool("isJump", false);
+        }
+
+        // Run Check
+        if (Input.GetAxis("Fire3") != 0) {
+            anim.SetBool("isRun", true);
+        }
+        else {
+            anim.SetBool("isRun", false);
+        }
+
+        // Walk Check
+        if (translation != 0)
+        {
+        	anim.SetBool("isWalk", true);
+        	anim.SetFloat("speed", translation);
+        }
+        else
+        {
+        	anim.SetBool("isWalk", false);
+        	anim.SetFloat("speed", 0);
+        }
     }
 }
