f0da46ce1102f2a6ca5f277c17edbef9a30746cc
AlfredPros <alfredkuhlman@yahoo.co.id>
2022-12-15 22:46:09 +0700

    Camera Collision

--- Custom/Camera/CameraNans.cs
+++ Custom/Camera/CameraNans.cs
@@ -9,14 +9,26 @@ public class CameraNans : MonoBehaviour
 
     protected Vector3 _LocalRotation;
     protected float _CameraDistance = 2f;
+    protected float _CameraDistanceNow = 2f;  // Is affected by collision
 
     public float MouseSensitivity = 4f;
     public float ScrollSensitvity = 2f;
     public float OrbitDampening = 12f;
     public float ScrollDampening = 8f;
-    public float minYRotation = -15f;
+    public float minYRotation = -30f;
     public float maxYRotation = 90f;
 
+    // Collision
+    public float minDistance = 1.5f;
+	public float smooth = 16.0f;
+	Vector3 dollyDir;
+	public Vector3 dollyDirAdjusted;
+
+
+    // Initialization
+    void Awake () {
+		dollyDir = transform.localPosition.normalized;
+	}
 
     // Start is called before the first frame update
     void Start()
@@ -52,14 +64,26 @@ public class CameraNans : MonoBehaviour
             this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 3f);
         }
         
+        // Collision
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * this._CameraDistance );
+        RaycastHit hit;
 
-        // Actual Camera Rig Transformations
+        if (Physics.Linecast (this._XForm_Parent.position, desiredCameraPos, out hit)) {
+            this._CameraDistanceNow = Mathf.Clamp((hit.distance * 0.87f), minDistance, 3.0f);
+        }
+        else {
+            // Reset distance if not collided
+            this._CameraDistanceNow = Mathf.Lerp(this._CameraDistance, 2.0f, Time.deltaTime * smooth);
+        }
+
+        // Actual Camera Rig Transformations For Rotation and Zoom in/out
         Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
         this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
 
-        if ( this._XForm_Camera.localPosition.z != this._CameraDistance * -1f )
+        if ( this._XForm_Camera.localPosition.z != this._CameraDistanceNow * -1f )
         {
-            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
+            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistanceNow * -1f, Time.deltaTime * ScrollDampening));
         }
+
     }
 }
